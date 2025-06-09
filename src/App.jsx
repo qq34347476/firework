39b7cbca4da3e7dc6effe5fdd796bdc2278b5b51
34347476@qq.com <34347476@qq.com>
2025-06-09 15:49:25 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import Footer from './components/Footer/Footer';
 
 import Home from './page/Home';
 import ContactUs from './page/ContactUs';
-<<<<<<< HEAD
 import Solutions from './page/Solutions';
-=======
 import ShoppableVideo from './page/ShoppableVideo'
->>>>>>> 49d95ba04798875813081351ac607ccc2d1f398b
 
 import './App.css'
 
@@ -19,11 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact-us" element={<ContactUs />} /> 
-<<<<<<< HEAD
         <Route path="/solutions" element={<Solutions />} /> 
-=======
         <Route path="/shoppable-video" element={<ShoppableVideo />} /> 
->>>>>>> 49d95ba04798875813081351ac607ccc2d1f398b
       </Routes>
       <Footer />
     </>
